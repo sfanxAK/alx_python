79c6904585fb2ea78b67ba3f611493ba9c08cf8e
sfanxAK <soufianeakhiat@gmail.com>
2023-11-26 03:18:04 +0100

    Updated

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,4 +1,4 @@
 #program that prints numbers from 0 to 99
 for i in range(100):
-    last = ', ' if i < 99 else ''
-    print('{:02},'.format(i, last), end=" ")
+    separator = ',' if i < 99 else ''
+    print('{:02}{}'.format(i, separator), end=" ")

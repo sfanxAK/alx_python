6769b1d45d306555f585200264cd64a72aa2dfdd
sfanxAK <soufianeakhiat@gmail.com>
2023-11-26 02:47:52 +0100

    update

--- python-hello_world/4-print_hexa.py
+++ python-hello_world/4-print_hexa.py
@@ -1,4 +1,5 @@
 #This program prints all numbers from 0 to 98 in decimal and in hexadecimal
-for i in range(99):
-    print(f'{i} = {hex(i)}')
+for i in range(1, 99):
+    x = f'{i} = {hex(i)}'
+    print(x)
 

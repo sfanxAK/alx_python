e3cb71848f09b446219532da10ab408fa4535290
sfanxAK <soufianeakhiat@gmail.com>
2023-11-26 17:41:48 +0100

    update

--- python-functions/0-sum.py
+++ python-functions/0-sum.py
@@ -3,8 +3,8 @@ def add(a, b):
     return a + b
 
 result_1 = add(1, 2)
-result_2 = add(100, -2)
-result_3 = add(-100, -2)
 print(result_1)
+result_2 = add(100, -2)
 print(result_2)
+result_3 = add(-100, -2)
 print(result_3)

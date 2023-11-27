1651f88f89522d68d6021f4ef8baa3ddd597d671
sfanxAK <soufianeakhiat@gmail.com>
2023-11-27 01:36:21 +0100

    update

--- python-functions/6-password.py
+++ python-functions/6-password.py
@@ -4,8 +4,8 @@ def validate_password(password):
     if len(password) < 8:
         return False
     
-    has_lower = any(char.islower() for char in password)
-    has_upper = any(char.isupper() for char in password)
-    has_digit = any(char.isdigit() for char in password)
+    lower = any(char.islower() for char in password)
+    upper = any(char.isupper() for char in password)
+    digit = any(char.isdigit() for char in password)
 
-    return has_lower and has_upper and has_digit
+    return lower and upper and digit

7924aa7ae78c3b49496d5cc231b7631f6bfe98db
sfanxAK <soufianeakhiat@gmail.com>
2023-12-02 18:34:00 +0100

    add

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -1,13 +1,18 @@
 def safe_print_division(a, b):
     try:
         result = a / b
+        print('Inside result: {:.2f}'.format(result))
     except ZeroDivisionError:
-        print('Inside result: {:d}'.format(result))
+        result = None
+        print('{:d} / {:d} = None'.format(a, b))
     finally:
-        if result != 0:
-          print('{:d} / {:d} = {:.2f}'.format(a, b, result))
-        else:
-          print('{:d} / {:d} = {:.2f}'.format(a, b, result))
+        print('{:d} / {:d} = {:.2f}'.format(a, b, result))
+
+            
 
 
+# Example usage
 
+a = int(input("Enter the numerator: "))
+b = int(input("Enter the denominator: "))
+safe_print_division(a, b)

bda8bf83a9d6781be9f0c90ca141cd213c66c34e
sfanxAK <soufianeakhiat@gmail.com>
2023-12-02 15:37:54 +0100

    add

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -3,14 +3,15 @@ import sys
 def print_arguments():
     arguments = sys.argv[1:]
     num_arguments = len(arguments)
-    print(f"Number of argument{'s' if num_arguments != 1 else ''}: {num_arguments}", end='')
+    
 
     if num_arguments > 0:
-        print(", followed by:")
+        print(num_arguments,"argument: ")
         for i, arg in enumerate(arguments, start=1):
             print(f"{i}: {arg}")
     else:
-        print(".")
+      print(num_arguments,"argument.")
+        
 
 if __name__ == "__main__":
     print_arguments()

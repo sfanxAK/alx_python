87ad589eadbe06aa10d10718d486a5db30205a74
sfanxAK <soufianeakhiat@gmail.com>
2024-01-14 17:30:11 +0100

    add

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -5,7 +5,7 @@ config_connect = Server.connect(host='localhost', port=3306, user=argv[1], passw
 
 cursor = config_connect.cursor
 
-cursor.execute("SELECT * FROM cities ORDER BY id ASC")
+cursor.execute("SELECT * FROM states AND cities ORDER BY id ASC")
 
 rows = cursor.fetchall()
 

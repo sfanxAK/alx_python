c0931c1ebf1306f9bccac16f8d60aeb09dab8cd5
sfanxAK <soufianeakhiat@gmail.com>
2024-01-14 01:34:43 +0100

    update

--- python-object_relational_mapping/ 2-my_filter_states.py
+++ python-object_relational_mapping/ 2-my_filter_states.py
@@ -1,5 +1,5 @@
-import MySQLdb as Server
 from sys import argv
+import MySQLdb as Server
 
 config_connect = Server.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], database=argv[3])
 

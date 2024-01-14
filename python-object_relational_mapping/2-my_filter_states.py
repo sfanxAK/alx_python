f6887cfd4e767a583d2e3540739f7e0b5a8e775b
sfanxAK <soufianeakhiat@gmail.com>
2024-01-14 16:33:33 +0100

    add

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,14 +1,19 @@
-from sys import argv
 import MySQLdb as Server
+from sys import argv
 
 config_connect = Server.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], database=argv[3])
 
 cursor = config_connect.cursor()
 
-cursor.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(argv[4]))
+# Use parameterized query to avoid SQL injection
+query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+cursor.execute(query, (argv[4],))
 
 rows = cursor.fetchall()
 
 for row in rows:
     if row[1][0] == 'N':
         print(row)
+
+    
+

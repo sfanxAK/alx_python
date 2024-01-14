43c2ec05a5f343cc12af3dad65b6571eb2c4a0b0
sfanxAK <soufianeakhiat@gmail.com>
2024-01-14 01:06:58 +0100

    add

---
+++ python-object_relational_mapping/ 2-my_filter_states.py
@@ -0,0 +1,15 @@
+import MySQLdb as Server
+from sys import argv
+
+config_connect = Server.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], database=argv[3])
+
+cursor = config_connect.cursor()
+
+cursor.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(argv[4]))
+cursor.execute(query, (state_name,))
+
+rows = cursor.fetchall()
+
+for row in rows:
+    print(row)
+
---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,13 @@
+import MySQLdb as Server
+from sys import argv
+
+config_connect = Server.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], database=argv[3])
+
+cursor = config_connect.cursor()
+
+cursor.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+
+rows = cursor.fetchall()
+
+for row in rows:
+    print(row)

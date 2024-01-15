676caa34142b814203910102be78238c5b232623
sfanxAK <soufianeakhiat@gmail.com>
2024-01-15 18:55:55 +0100

    add

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -7,7 +7,9 @@ cursor = config_connect.cursor()
 
 query = '''SELECT cities.name FROM cities
           JOIN states ON cities.state_id = states.id
-          WHERE state_id = (SELECT id FROM states WHERE name = %s ORDER BY cities.id ASC'''
+          WHERE state_id = (SELECT id FROM states WHERE name = %s)
+          ORDER BY cities.id ASC'''
+
 
 cursor.execute(query, (argv[4],))
 

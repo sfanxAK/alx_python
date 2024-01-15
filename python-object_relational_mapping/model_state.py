ed81eefe2cce7386815403d8a49a12f8861213ec
sfanxAK <soufianeakhiat@gmail.com>
2024-01-15 21:39:07 +0100

    add

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -4,13 +4,15 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 class State(Base):
-  """ created State class"""
-  __init__ = 'states'
-  id = Column(String(128), primary_key=True)
-
-  def display_state(self):
-    print("ID: ", self.id)
-
+    """ created State class"""
+    __tablename__ = 'states'
+    id = Column(Integer, 
+                primary_key=True, 
+                autoincrement=True, 
+                unique=True, 
+                nullable=False)
+    name =  Column(String(128),
+                   nullable=False)
 
   
 
